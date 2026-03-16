30b5a569b96c3d675f5183b1ffd5e480b14fe384
harshitkukreja11 <harshit.kukreja11@gmail.com>
2026-03-16 19:34:34 +0530

    fix prisma build issue

--- src/lib/prisma.js
+++ src/lib/prisma.js
@@ -2,26 +2,26 @@ import { PrismaClient } from "@prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
 import { Pool } from "pg";
 
-const connectionString = process.env.DATABASE_URL;
+let prisma;
 
-if (!connectionString) {
-  throw new Error("DATABASE_URL is not set");
+if (!globalThis.prisma) {
+  const connectionString = process.env.DATABASE_URL;
+
+  if (!connectionString) {
+    console.warn("DATABASE_URL not found during build");
+  } else {
+    const pool = new Pool({
+      connectionString,
+    });
+
+    const adapter = new PrismaPg(pool);
+
+    prisma = new PrismaClient({
+      adapter,
+    });
+
+    globalThis.prisma = prisma;
+  }
 }
 
-const pool = new Pool({
-  connectionString,
-});
-
-const adapter = new PrismaPg(pool);
-
-const globalForPrisma = globalThis;
-
-export const prisma =
-  globalForPrisma.prisma ||
-  new PrismaClient({
-    adapter,
-  });
-
-if (process.env.NODE_ENV !== "production") {
-  globalForPrisma.prisma = prisma;
-}
+export const prisma = globalThis.prisma;
